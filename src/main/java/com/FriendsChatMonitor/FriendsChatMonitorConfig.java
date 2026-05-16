305acf87d633a689a77a33d03ab4b452789a81d4
Nick White <nhwhite3118@gmail.com>
2026-05-16 16:06:05 -0500

    Hide API key, added warning about 3rd party server

--- src/main/java/com/FriendsChatMonitor/FriendsChatMonitorConfig.java
+++ src/main/java/com/FriendsChatMonitor/FriendsChatMonitorConfig.java
@@ -11,7 +11,9 @@ public interface FriendsChatMonitorConfig extends Config
         keyName = "apiKey",
         name = "SaaS API Key",
         description = "Your unique API key from the dashboard",
-        position = 1
+        position = 1,
+        secret = true,
+        warning = "This feature submits your IP address to a 3rd-party server not controlled or verified by RuneLite developers"
     )
     default String apiKey() { return ""; }
 
